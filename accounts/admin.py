41d2cd68810fbd0536aa91d0dd882ac101970974
Forhath Hosain Ome <omehasan3@gmail.com>
2025-09-14 16:00:57 +0600

    initial setup-02

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import Account
 # Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ['first_name', 'last_name', 'phone_number', 'is_admin', 'is_superuser', 'username', 'email', 'is_staff', 'is_active', 'date_joined', 'last_login']
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+admin.site.register(Account)
